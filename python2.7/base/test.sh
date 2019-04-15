da0b6771cc12e8e1066a115c3f72a90c100108ac
zhouwei <m15038960016@163.com>
2019-04-15 11:12:14 +0800

    update

--- python2.7/base/test.sh
+++
@@ -1,9 +0,0 @@
-tiss=$(cat config.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
-echo ${tiss}
-
-
-
-
-if [[ $tiss == "1.0.3" ]]; then
-  echo 'dd'
-fi
